48e2367483051e82ec38fa1bfc59cbf92479a13d
tareasr <tareasrojano@gmail.com>
2025-03-07 10:37:29 -0600

    rutas en flask01

--- flask01/app.py
+++ flask01/app.py
@@ -1,7 +1,26 @@
 from flask import Flask
+
 app = Flask(__name__)
-@app.route('/')
+
+@app.route('/saludar')
 def hola_mundo():
-    return 'Hola mundo!'
-if __name__== '__main__':
-    app.run(host='0.0.0.0')
+    return 'Hola Mundo!'
+
+@app.route('/adios')
+def adios_mundo():
+    return 'Adios Mundo!'
+
+@app.route('/hola')
+def hola_html():
+    return '<h1 style="color:red;">Hola!!</h1>'
+
+@app.route('/json')
+def json():
+    return '{"nombre":"john"}'
+
+@app.route('/xml')
+def xml():
+    return '<?xml version="1.0"?><nombre>john</nombre>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
